9b76c8e0006adaa3a02ff8fd5058f44cc5f2f13f
LARISSA FIGUEREDO MARIANO <26171619@edu119.local>
2026-08-18 11:47:50 -0300

    Salvando alterações

--- 01-funcoes/06-function-expression.js
+++ 01-funcoes/06-function-expression.js
@@ -6,5 +6,10 @@ const somarAnonima = function (a, b){
     return a + b;
 }
 
+const somarArrowFuction = (10,5) => {
+    return a + b;
+}
+
 console.log(somarTradicional(10,5));
 console.log(somarAnonima(10,5));
+console.log(somarArrowFuction(10,5));
