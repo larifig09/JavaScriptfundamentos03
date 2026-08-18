f1af073abe9f6fd633863573f5566aa194932849
LARISSA FIGUEREDO MARIANO <26171619@edu119.local>
2026-08-18 09:40:11 -0300

    Salvando alterações

---
+++ 01-funcoes/02-parametros.js
@@ -0,0 +1,19 @@
+// function apresentar(nome){
+//     console.log(`Olá, ${nome}`);
+// }
+
+// apresentar("Larissa");
+
+function somar(a, b){
+    console.log(`${a} + ${b} = ${a+b}`);
+}
+
+somar(8, 15);
+
+function apresentar(nome = "Visitante"){
+console.log(`Olá, ${nome}`);
+}
+apresentar();
+apresentar("Larissa");
+
+
